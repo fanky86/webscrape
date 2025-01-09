3603c8321e48614338202b49275ed75fa7b15640
MR.ExceFaN <106883384+fanky86@users.noreply.github.com>
2025-01-09 11:27:29 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # -----------------------[ DEFF SCRAPT METODE ]--------------------#
-import sys,bs4, requests,rich,datetime,os
+import sys,bs4, requests,rich,datetime,os, random 
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
 from itertools import count
